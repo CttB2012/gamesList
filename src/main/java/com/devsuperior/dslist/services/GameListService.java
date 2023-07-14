e81258836789b689089b99f7a5bd6dadb339f145
Wiliam GM <gomesruivo@gmail.com>
2023-07-14 13:14:52 -0300

    Metodo para a troca de posicao da lista implementado

--- src/main/java/com/devsuperior/dslist/services/GameListService.java
+++ src/main/java/com/devsuperior/dslist/services/GameListService.java
@@ -2,9 +2,12 @@ package com.devsuperior.dslist.services;
 
 import com.devsuperior.dslist.dto.GameListDto;
 import com.devsuperior.dslist.entities.GameList;
+import com.devsuperior.dslist.projections.GameMinProjection;
 import com.devsuperior.dslist.repositories.GameListRepository;
+import com.devsuperior.dslist.repositories.GameRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.stream.Collectors;
@@ -15,6 +18,9 @@ public class GameListService {
     @Autowired
     private GameListRepository gameListRepository;
 
+    @Autowired
+    private GameRepository gameRepository;
+
 
     public List<GameListDto> findLists(){
 
@@ -22,5 +28,23 @@ public class GameListService {
 
         List<GameListDto> dto = result.stream().map(list -> new GameListDto(list)).collect(Collectors.toList());
         return dto;
+    }
+
+    @Transactional
+    public void move(Long listId, int sourceIndex, int destinationIndex){
+
+        List<GameMinProjection> list = gameRepository.searchByList(listId);
+
+        GameMinProjection obj = list.remove(sourceIndex);
+
+        list.add(destinationIndex, obj);
+
+        int min = sourceIndex < destinationIndex ? sourceIndex : destinationIndex;
+        int max = sourceIndex < destinationIndex ? destinationIndex : sourceIndex;
+
+        for (int i = min; i <=max  ; i++) {
+
+            gameListRepository.updateBelongingPosition(listId, list.get(i).getId(), i);
+        }
     }
 }
